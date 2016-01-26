a07d027d0a7675c82d91b790af14caf38b83e4ed
zOxta <zOxta@users.noreply.github.com>
2016-01-26 11:01:55 -0800

    Updated greplace to add different versions

--- greplace.sh
+++ greplace.sh
@@ -1,1 +1,5 @@
+# Ubuntu's version
+grep -rl "https://ajax.googleapis.com/ajax/libs/jquery" /in/this/directory | xargs sed -i 's/https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery/https:\/\/cdn.jsdelivr.net\/jquery/g'
+
+# OSX's version (notice the empty string after the -i argument)
 grep -rl "https://ajax.googleapis.com/ajax/libs/jquery" /in/this/directory | xargs sed -i '' 's/https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery/https:\/\/cdn.jsdelivr.net\/jquery/g'
